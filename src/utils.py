ba828e5ddbf541815ba2566c31973ebc7c646775
Alessandro <alessandro1.sanvito@mail.polimi.it>
2021-07-01 16:17:53 +0200

    Revert "feat: add schema"
    This reverts commit a05ae43db8b0a0470ac2a3beed08c4a4798bc64a.

--- src/utils.py
+++ src/utils.py
@@ -2,10 +2,9 @@ import json
 from io import TextIOBase
 from os import PathLike
 from typing import Union, IO
-from zquantum.core.utils import SCHEMA_VERSION
+
 
 def save_dict(to_save: dict, output_file: Union[TextIOBase, IO[str], str, PathLike]) -> None:
-    to_save["schema"] = SCHEMA_VERSION + "-dict"
     try:
         json.dump(to_save, output_file)
     except AttributeError:
@@ -19,7 +18,5 @@ def load_dict(input_file: Union[TextIOBase, IO[str], str, PathLike]) -> dict:
     except AttributeError:
         with open(input_file, "r") as input_file:
             to_load = json.load(input_file)
-
-    del to_load["schema"]
 
     return to_load
