da26f9d97be657ec0cfe17b07e6e9836e1782c6e
Alessandro <alessandro1.sanvito@mail.polimi.it>
2021-07-02 12:51:28 +0200

    feat: change setup

--- setup.py
+++ setup.py
@@ -1,20 +1,19 @@
 import setuptools
 
 setuptools.setup(
-    name="linear-regression-orquestra",
-    author="Alessandro Sanvito",
-    author_email="alessandro1.sanvito@mail.polimi.it",
-    description="Quantum linear regression on Orquestra.",
-    url="https://github.com/zapatacomputing/z-quantum-qubo",
-    package_dir={"": "src/python"},
-    classifiers=(
+    name                            = "linear-regression-orquestra",
+    version                         = "0.0.0",
+    author                          = "Alessandro Sanvito",
+    author_email                    = "alessandro1.sanvito@mail.polimi.it",
+    packages                        = setuptools.find_packages(),
+    classifiers                     = [
         "Programming Language :: Python :: 3",
         "Operating System :: OS Independent",
-    ),
-    install_requires=[
+    ],
+    install_requires = [
         "dimod",
         "dwave-neal",
         "pandas",
         "numpy"
-    ],
+   ],
 )
